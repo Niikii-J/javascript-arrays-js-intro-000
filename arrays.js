c79ce9c27de382f19ae9f6b370e7ab28e9565a97
Nikita Johnson <nikita_johnson@hotmail.com>
2020-10-21 20:00:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,12 @@
 var chocolateBars = ["snckers", "hundred grand", "kitkat", "skittles"]
 
+<<<<<<< HEAD
 function addElementToBeginningOfArray( arrayArg, elementArg ){
   let newArray = [...arrayArg]
   }
+=======
+function addElementToBeginningOfArray() {
+ var myPets = ["cat", "dog"]
+ return myPets = ["skunk" ...myPets]
+}
+>>>>>>> cc1af30f1e82722c1425bb844cfe33f5e5f15db0
